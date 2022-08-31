8561dfc5bc31c6cab014d6d5e431115f33791fcd
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-31 15:54:02 +0300

    Lesson 8: Working with Functions in TypeScript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,20 +1,18 @@
 "use strict";
-let age = 25;
-age = true;
-console.log(age);
-age = "hello";
-console.log(age);
-age = { name: "luigi" };
-console.log(age);
-let mixed = [];
-mixed.push(5);
-mixed.push("mario");
-mixed.push(false);
-console.log(mixed);
-let ninja;
-ninja = { name: "yoshi", age: 25 };
-console.log(ninja);
-console.log("M for MOO");
-console.log("gen gen");
-ninja = { name: 25, age: "yoshi" };
-console.log(ninja);
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
+// greet = 'hello';
+// greet = () => {
+//   console.log('hello, again');
+// }
+const add = (a, b, c = /*?*/ 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "ninja");
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+console.log(result);
