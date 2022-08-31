8ff517668f31ba802920cd258d6d38c46ac608d5
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-31 15:36:11 +0300

    Lesson 7: Working with the typescript config file

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,23 +1,20 @@
-let age: any = 25;
-
+"use strict";
+let age = 25;
 age = true;
 console.log(age);
 age = "hello";
 console.log(age);
 age = { name: "luigi" };
 console.log(age);
-
-let mixed: any[] = [];
-
+let mixed = [];
 mixed.push(5);
 mixed.push("mario");
 mixed.push(false);
 console.log(mixed);
-
-let ninja: { name: any; age: any };
-
+let ninja;
 ninja = { name: "yoshi", age: 25 };
 console.log(ninja);
-
+console.log("M for MOO");
+console.log("gen gen");
 ninja = { name: 25, age: "yoshi" };
 console.log(ninja);
---
+++ src/test.ts
@@ -0,0 +1,1 @@
+console.log("This was read from the TEST FILE");
